50ba5c8d2520fd05ba2844c2458ffbfbcb6c0628
paty0504 <thanhkhotinh@gmail.com>
2018-05-14 20:48:40 +0700

    '.'

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import *
 import mlab
+from mongoengine import *
 app = Flask(__name__)
+app.secret_key = 'a-super-secret-key'
 mlab.connect()
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        form = request.form
+        username = form['username']
+        password = form['password']
+        if username == 'paty0504' and password == 'paty0504':
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('login'))
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
